ecff1fb3cda39e508715618f32551de39ba4bc4b
selschicht <wechselschicht5@gmail.com>
2025-07-25 22:08:39 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/d-nerneckarsulm/', // ✅ BU SATIR EN ÜSTE EKLENMELİ
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
